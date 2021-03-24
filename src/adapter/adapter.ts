77485c2d7e302290a6e6b0f6433750fb3eea8337
Gilson Fernandes Batista de Souza <gilson@vitta.com.br>
2021-03-24 19:00:41 -0300

    feat: implementando recursos de limit e pagination

--- src/adapter/adapter.ts
+++ src/adapter/adapter.ts
@@ -1,201 +1,218 @@
-
 import {
-
-    createConnection,
-    ConnectionOptions,
-    Connection,
-    EntitySchema,
-    Repository,
-    FindOneOptions,
-    DeepPartial, FindConditions, FindManyOptions
+  createConnection,
+  ConnectionOptions,
+  Connection,
+  EntitySchema,
+  Repository,
+  FindOneOptions,
+  DeepPartial,
+  FindConditions,
+  FindManyOptions,
 } from 'typeorm';
+
 import * as Moleculer from 'moleculer';
 /* tslint:disable-next-line */
-import {Service, ServiceBroker} from 'moleculer';
+import { Service, ServiceBroker, Errors } from "moleculer";
 
 interface IndexMap {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 export class TypeOrmDbAdapter<T> {
-    public broker: Moleculer.ServiceBroker;
-    public service: Moleculer.Service;
-    public repository: Repository<T>;
-    public connection: Connection;
-    private opts: ConnectionOptions;
-    private entity: EntitySchema<T>;
+  public broker: Moleculer.ServiceBroker;
+  public service: Moleculer.Service;
+  public repository: Repository<T>;
+  public connection: Connection;
+  private opts: ConnectionOptions;
+  private entity: EntitySchema<T>;
 
-    constructor(opts: ConnectionOptions) {
-        this.opts = opts;
+  constructor(opts: ConnectionOptions) {
+    this.opts = opts;
+  }
+
+  public find(filters: any) {
+    return this.createCursor(filters, false);
+  }
+
+  public findOne(query: FindOneOptions) {
+    return this.repository.findOne(query);
+  }
+
+  public findById(id: number) {
+    return this.repository
+      .findByIds([id])
+      .then((result) => Promise.resolve(result[0]));
+  }
+
+  public findByIds(idList: any[]) {
+    return this.repository.findByIds(idList);
+  }
+
+  public count(filters = {}) {
+    return this.createCursor(filters, true);
+  }
+
+  public insert(entity: any) {
+    return this.repository.save(entity);
+  }
+
+  public create(entity: any) {
+    return this.insert(entity);
+  }
+
+  public insertMany(entities: any[]) {
+    return Promise.all(entities.map((e) => this.repository.create(e)));
+  }
+
+  public beforeSaveTransformID(entity: T, _idField: string) {
+    return entity;
+  }
+
+  public afterRetrieveTransformID(entity: T, _idField: string) {
+    return entity;
+  }
+
+  public init(broker: ServiceBroker, service: Service) {
+    this.broker = broker;
+    this.service = service;
+
+    const entityFromService = this.service.schema.model;
+    const isValid = !!entityFromService.constructor;
+
+    if (!isValid) {
+      throw new Errors.MoleculerError(
+        'The provided model should be a TypeORM entity model'
+      );
+    }
+    this.entity = entityFromService;
+  }
+
+  public connect() {
+    const connectionPromise = createConnection({
+      entities: [this.entity],
+      synchronize: true,
+      ...this.opts,
+    });
+    return connectionPromise.then((connection) => {
+      this.connection = connection;
+      this.repository = this.connection.getRepository(this.entity);
+    });
+  }
+
+  public disconnect() {
+    if (this.connection) {
+      return this.connection.close();
+    }
+    return Promise.resolve();
+  }
+
+  public updateMany(where: FindConditions<T>, update: DeepPartial<T>) {
+    const criteria: FindConditions<T> = { where } as any;
+    return this.repository.update(criteria, <any> update);
+  }
+
+  public updateById(id: number, update: { $set: DeepPartial<T> }) {
+    return this.repository.update(id, <any> update.$set);
+  }
+
+  public removeMany(where: FindConditions<T>) {
+    const { useSoftDelete } = this.service.schema;
+
+    if (useSoftDelete) {
+      return this.repository.softDelete(where);
     }
 
-    public find(filters: any) {
-        return this.createCursor(filters, false);
+    return this.repository.delete(where);
+  }
+
+  public removeById(id: number) {
+    const { useSoftDelete } = this.service.schema;
+    const result = useSoftDelete
+      ? this.repository.softDelete(id)
+      : this.repository.delete(id);
+
+    return result.then(() => {
+      return { id };
+    });
+  }
+
+  public clear() {
+    return this.repository.clear();
+  }
+
+  public entityToObject(entity: T) {
+    return entity;
+  }
+
+  public createCursor(params: any, isCounting: boolean = false) {
+    if (params) {
+      const query: FindManyOptions<T> = {
+        where: params.query || {},
+      };
+      this._enrichWithOptionalParameters(params, query);
+
+      return this._runQuery(isCounting, query);
     }
 
-    public findOne(query: FindOneOptions) {
-        return this.repository.findOne(query);
+    return this._runQuery(isCounting);
+  }
+
+  private _runQuery(isCounting: boolean, query?: FindManyOptions<T>) {
+    if (isCounting) {
+      return this.repository.count(query);
+    } else {
+      return this.repository.find(query);
+    }
+  }
+
+  private _enrichWithOptionalParameters(
+    params: any,
+    query: FindManyOptions<T>
+  ) {
+    if (params.sort) {
+      const sort = this.transformSort(params.sort);
+
+      if (sort) {
+        query.order = sort as any;
+      }
     }
 
-    public findById(id: number) {
-        return this.repository.findByIds([id]).then((result) => Promise.resolve(result[0]));
+    if (params.relations && Array.isArray(params.relations)) {
+      query.relations = params.relations;
     }
 
-    public findByIds(idList: any[]) {
-        return this.repository.findByIds(idList);
+    if (Number.isInteger(Number(params.offset)) && Number(params.offset) > 0) {
+      query.skip = Number(params.offset);
     }
 
-    public count(filters = {}) {
-        return this.createCursor(filters, true);
+    if (Number.isInteger(Number(params.limit)) && Number(params.limit) > 0) {
+      query.take = Number(params.limit);
     }
+  }
 
-    public insert(entity: any) {
-        return this.repository.save(entity);
+  private transformSort(
+    paramSort: string | string[]
+  ): { [columnName: string]: 'ASC' | 'DESC' } {
+    let sort = paramSort;
+    if (typeof sort === 'string') {
+      sort = sort.replace(/,/, ' ').split(' ');
     }
-
-    public create(entity: any) {
-        return this.insert(entity);
-    }
-
-    public insertMany(entities: any[]) {
-        return Promise.all(entities.map((e) => this.repository.create(e)));
-    }
-
-    public beforeSaveTransformID(entity: T, _idField: string) {
-        return entity;
-    }
-
-    public afterRetrieveTransformID(entity: T, _idField: string) {
-        return entity;
-    }
-
-    public init(broker: ServiceBroker, service: Service) {
-        this.broker = broker;
-        this.service = service;
-        const entityFromService = this.service.schema.model;
-        const isValid = !!entityFromService.constructor;
-        if (!isValid) {
-            throw new Error('if model provided - it should a typeorm repository');
+    if (Array.isArray(sort)) {
+      const sortObj: IndexMap = {};
+      sort.forEach((s) => {
+        if (s.startsWith('-')) {
+          sortObj[s.slice(1)] = 'DESC';
+        } else {
+          sortObj[s] = 'ASC';
         }
-        this.entity = entityFromService;
-
+      });
+      // @ts-ignore
+      return sortObj;
     }
 
-    public connect() {
-        const connectionPromise = createConnection({
-            entities: [this.entity],
-            synchronize: true,
-            ...this.opts
-
-        });
-        return connectionPromise.then((connection) => {
-            this.connection = connection;
-            this.repository = this.connection
-                .getRepository(this.entity);
-        });
+    if (typeof sort === 'object') {
+      return sort;
     }
-
-    public disconnect() {
-        if (this.connection) {
-            return this.connection.close();
-        }
-        return Promise.resolve();
-    }
-
-    public updateMany(where: FindConditions<T>, update: DeepPartial<T>) {
-        const criteria: FindConditions<T> = {where} as any;
-        return this.repository.update(criteria,<any>  update);
-    }
-
-    public updateById(id: number, update: { $set: DeepPartial<T> }) {
-        return this.repository.update(id, <any> update.$set);
-    }
-
-    public removeMany(where: FindConditions<T>) {
-        return this.repository.delete(where);
-    }
-
-    public removeById(id: number) {
-        const result = this.repository.delete(id);
-        return result.then(() => {
-            return {id};
-        });
-    }
-
-    public clear() {
-        return this.repository.clear();
-    }
-
-    public entityToObject(entity: T) {
-        return entity;
-    }
-
-    public createCursor(params: any, isCounting: boolean = false) {
-        if (params) {
-            const query: FindManyOptions<T> = {
-                where: params.query || {}
-            };
-            this._enrichWithOptionalParameters(params, query);
-
-            return this._runQuery(isCounting, query);
-        }
-
-        return this._runQuery(isCounting);
-    }
-
-    private _runQuery(isCounting: boolean, query?: FindManyOptions<T>) {
-        if (isCounting) {
-            return this.repository.count(query);
-        }
-        else {
-            return this.repository.find(query);
-        }
-    }
-
-    private _enrichWithOptionalParameters(params: any, query: FindManyOptions<T>) {
-        if (params.search) {
-            throw new Error('Not supported because of missing or clause meanwhile in typeorm');
-        }
-
-        if (params.sort) {
-            const sort = this.transformSort(params.sort);
-            if (sort) {
-                query.order = sort as any;
-            }
-        }
-
-        if (Number.isInteger(params.offset) && params.offset > 0) {
-            query.skip = params.offset;
-        }
-
-        if (Number.isInteger(params.limit) && params.limit > 0) {
-            query.take = params.limit;
-        }
-    }
-
-    private transformSort(paramSort: string | string[]): { [columnName: string]: ('ASC' | 'DESC') } {
-        let sort = paramSort;
-        if (typeof sort === 'string') {
-            sort = sort.replace(/,/, ' ').split(' ');
-        }
-        if (Array.isArray(sort)) {
-            const sortObj: IndexMap = {};
-            sort.forEach((s) => {
-                if (s.startsWith('-')) {
-                    sortObj[s.slice(1)] = 'DESC';
-                }
-                else {
-                    sortObj[s] = 'ASC';
-                }
-            });
-            // @ts-ignore
-            return sortObj;
-        }
-
-        if (typeof sort === 'object') {
-            return sort;
-        }
-        return {};
-    }
-
+    return {};
+  }
 }
